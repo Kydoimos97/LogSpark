2703c6b676edd9269f5b0c7ce46c13af4ae13e13
Kydoimos97 <willemvanderschans97@gmail.com>
2026-01-06 02:01:27 -0700

    Add Docstring

--- logspark/_Internal/Func/configure_handler_traceback_policy.py
+++ logspark/_Internal/Func/configure_handler_traceback_policy.py
@@ -5,11 +5,15 @@ from ...Types import TracebackOptions
 
 def configure_handler_traceback_policy(handler: logging.Handler, policy: TracebackOptions) -> None:
     """
-    Configure handler to include traceback policy in log records
+    Configure handler to include traceback policy in log records.
+    
+    Adds a filter to the handler that injects the specified traceback policy
+    into all log records processed by this handler. This allows downstream
+    components to determine how to render exception tracebacks.
 
     Args:
-        handler: Handlers to configure
-        policy: Traceback policy to apply
+        handler: Handler to configure with the traceback policy.
+        policy: Traceback policy to apply to all records from this handler.
     """
 
     # Add a filter that injects traceback policy into log records
