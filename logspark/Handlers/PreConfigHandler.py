b88fd7afa78f15c0f4835488f58274795694b7f3
Kydoimos97 <willemvanderschans97@gmail.com>
2026-01-22 11:02:59 -0700

    Rename PreConfig.py to PreConfigHandler.py and convert to PreConfigHandler class
    - Rename logspark/Handlers/PreConfig.py to logspark/Handlers/PreConfigHandler.py
    - Replace pre_config_handler function with PreConfigHandler subclass of StreamHandler
    - Configure stream to sys.stdout or devnull if silenced, set consistent formatter in constructor

---
+++ logspark/Handlers/PreConfigHandler.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+
+from .._Internal.Func import get_devnull
+from .._Internal.State import is_silenced_mode
+from ..Types.Protocol import SupportsWrite
+
+
+class PreConfigHandler(logging.StreamHandler[SupportsWrite]):
+    def __init__(self, stream: SupportsWrite = sys.stdout) -> None:
+        stream = stream or sys.stdout
+        if is_silenced_mode():
+            stream = get_devnull()
+
+        super().__init__(stream=stream)
+        fmt = logging.Formatter(
+            fmt="(PreConfig) %(asctime)-8s %(levelname)-8s - %(filename)s:%(lineno)d -> %(message)s",
+            datefmt="%H:%M:%S",
+        )
+        self.setFormatter(fmt)
